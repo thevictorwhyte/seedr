0df1a45fb6293283ae627a4befa56dbdcbd298e4
Victor Whyte <victordavidwhyte@gmail.com>
2024-11-08 22:25:46 +0100

    feat: add passport

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.port ?? 3001);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
